f98a5224be474099a235432d27fc58801a030ae1
iemong <kiyoshidainagon@gmail.com>
2021-03-08 00:03:09 +0900

    その日の日付で睡眠時間を取れるように

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import {config} from "https://deno.land/x/dotenv/mod.ts";
+import {format} from "https://deno.land/x/date_fns/index.js";
 
-const { token } = config();
+const {token} = config();
 
-const res = await fetch("https://api.fitbit.com/1/user/-/profile.json", {
-  headers: new Headers({
-    method: "GET",
-    "Authorization": `Bearer ${token}`,
-  }),
-});
+const date = format(new Date(), "yyyy-MM-dd", {});
+
+const res = await fetch(
+    `https://api.fitbit.com/1.2/user/-/sleep/date/${date}.json`,
+    {
+        headers: new Headers({
+            method: "GET",
+            "Authorization": `Bearer ${token}`,
+        }),
+    },
+);
 
 const json = await res.json();
 
